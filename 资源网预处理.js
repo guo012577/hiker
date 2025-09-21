d9d712b55445a614608b488b472c1249a39995e2
guo012577 <132767548+guo012577@users.noreply.github.com>
2025-09-21 23:51:53 +0800

    Add files via upload

---
+++ 资源网预处理.js
@@ -0,0 +1,22 @@
+let obj = {
+        模板:'https://raw.githubusercontent.com/guo012577/hiker/refs/heads/main/zyw.js',
+};
+ua = (typeof (ua) !== 'undefined' && ua) ? ua :'';
+type = (typeof (type) !== 'undefined' && type) ? type :'';
+jm = (typeof (jm) !== 'undefined' && jm) ? jm :'';
+debug = (typeof (debug) !== 'undefined' && debug) ? debug :'';
+timeout = (typeof (timeout) !== 'undefined' && timeout && !!Number(timeout)) ? timeout :'5000';
+if(ua){
+    Object.assign(obj,{ua:ua});
+}
+if(type){
+    Object.assign(obj,{type:type});
+}
+if(jm){
+    Object.assign(obj,{jm:jm});
+}
+if(debug){
+    Object.assign(obj,{debug:true});
+}
+Object.assign(obj,{timeout:parseInt(timeout)});
+initConfig(obj);
