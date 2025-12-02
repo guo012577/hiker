1b67a70a7dfce0544f1faedcfe77df5deef028d0
guo012577 <132767548+guo012577@users.noreply.github.com>
2025-12-02 23:01:56 +0800

    Update 预处理.js

--- 预处理.js
+++ 预处理.js
@@ -1,12 +1,5 @@
-var dr_jsRoot = 'https://raw.githubusercontent.com/guo012577/hiker/refs/heads/main/';
-try {
-    const {getApi} = $.require("hiker://page/utiliy?rule=道长仓库Pro");
-    var 模板 = getApi('importUrl')+"2505";
-    require(模板);
-    预处理(模板)
-}catch (e) {
-    log('预处理执行失败:'+e.message);
-    var 模板 = dr_jsRoot+'dr.js';
-    require(模板);
-    预处理(模板)
-}
+var dr_jsRoot = 'https://gh-proxy.com/https://raw.githubusercontent.com/guo012577/hiker/refs/heads/main/';
+var 模板 = dr_jsRoot+'dr.js';
+require(模板);
+预处理(模板)
+
