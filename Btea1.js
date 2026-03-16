3f60a6c335bb872cacd7ce9f3162ab7a6cdbd780
guo012577 <132767548+guo012577@users.noreply.github.com>
2026-03-16 18:44:37 +0800

    Update Btea1.js

--- Btea1.js
+++ Btea1.js
@@ -1691,7 +1691,7 @@ putVar('小程序名', MY_RULE.title);
   if(getItem("shunsou","on")=="on"){ 
 	    var 本地 = 'hiker://files/rules/dzHouse/html/顺搜.html';
        if(fileExist(本地) == false) {
-			var 远程x5 = request('https://gh-proxy.com/https://raw.githubusercontent.com/guo012577/hiker/master/顺搜.html');
+			var 远程x5 = request('https://htmlpreview.github.io/?https://github.com/guo012577/hiker/blob/main/%E9%A1%BA%E6%90%9C.html');
 			if (远程x5.indexOf("search_bg")>0) {
 				   writeFile(本地, 远程x5);
 			} else {
