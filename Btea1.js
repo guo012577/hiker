0e63dc670d2fcb58ec1482dd5d597f09fa9ff6b7
guo012577 <132767548+guo012577@users.noreply.github.com>
2026-03-16 19:44:08 +0800

    Update Btea1.js

--- Btea1.js
+++ Btea1.js
@@ -3189,8 +3189,12 @@ try{
         }
             }
 //log('html='+html) 
-if(getItem("通免")=="通免"){def_lazy = config.通免}else if(getItem("通免")=="道长通免"){def_lazy=config.道长通免}else if(getItem("通免")=="香免"){def_lazy=config.香免} ;
-lazy=lazy||def_lazy;
+var def_lazy = getItem("通免")=="通免" ? config.通免 
+             : getItem("通免")=="道长通免" ? config.道长通免 
+             : getItem("通免")=="香免" ? config.香免 
+             : config.通免;
+lazy = lazy || def_lazy;
+
 /* var html = request(MY_URL, {headers: { "User-Agent": MOBILE_UA } })   */
         putMyVar('tab_text', _tab_text);
         putVar('_list_id', _list_id);
@@ -3526,8 +3530,11 @@ B: function(d,p,lazy,html) {
     指定ua=(typeof(指定ua)==='undefined'||!指定ua)?false:指定ua;
     d=d;
     html=html;
-    if(getItem("通免")=="通免"){def_lazy = config.通免}else if(getItem("通免")=="道长通免"){def_lazy=config.道长通免}else if(getItem("通免")=="香免"){def_lazy=config.香免} ;
-    lazy=lazy||def_lazy;
+    var def_lazy = getItem("通免")=="通免" ? config.通免 
+             : getItem("通免")=="道长通免" ? config.道长通免 
+             : getItem("通免")=="香免" ? config.香免 
+             : config.通免;
+lazy = lazy || def_lazy;
     let ua = config.ua==='手机'?MOBILE_UA:PC_UA;
     if(config.指定ua){
         ua = config.指定ua
