a3c1aa1d424f79b46b6612d7010a36cdcf736c02
guo012577 <132767548+guo012577@users.noreply.github.com>
2026-03-16 15:57:53 +0800

    Update Btea1.js

--- Btea1.js
+++ Btea1.js
@@ -3196,7 +3196,7 @@ try{
 //log('html='+html) 
 if(getItem("通免")=="道长通免"){def_lazy=config.道长通免}
 else if(getItem("通免")=="香免"){def_lazy=config.香免}
-else{def_lazy = config.通免};
+else if(getItem("通免")=="通免"){def_lazy = config.通免};
 lazy=lazy||def_lazy;
 /* var html = request(MY_URL, {headers: { "User-Agent": MOBILE_UA } })   */
         putMyVar('tab_text', _tab_text);
@@ -3535,7 +3535,7 @@ B: function(d,p,lazy,html) {
     html=html;
     if(getItem("通免")=="道长通免"){def_lazy=config.道长通免}
 	else if(getItem("通免")=="香免"){def_lazy=config.香免}
-    elseif(getItem("通免")=="通免"){def_lazy = config.通免};
+    else if(getItem("通免")=="通免"){def_lazy = config.通免};
     lazy=lazy||def_lazy;
     let ua = config.ua==='手机'?MOBILE_UA:PC_UA;
     if(config.指定ua){
